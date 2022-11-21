234dad75e66f3cd911ae739f486a7d9a32a22768
test_2022 <prudkyi87@gmail.com>
2022-11-21 16:08:54 +0100

    Start

--- themes/prdk/footer.php
+++ themes/prdk/footer.php
@@ -11,7 +11,7 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
+	<footer id="colophon" class="site-footer" style="background-color: #54ce16">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'prdk' ) ); ?>">
 				<?php
